d7aa00bf6dbba3852d7adf98671d186c2201f891
Szymon Rynkowski <rynkowski.sz@gmail.com>
2021-11-27 23:12:20 +0100

    nicer transaction building in importers

--- app/src/main/java/com/objectzilla/importer/SantanderImporter.java
+++ app/src/main/java/com/objectzilla/importer/SantanderImporter.java
@@ -11,14 +11,14 @@ public class SantanderImporter implements Importer{
 
     @Override
     public Transaction importLine(List<String> line) {
-        Transaction.TransactionBuilder transactionBuilder = new Transaction.TransactionBuilder();
-        transactionBuilder.operationDate(LocalDate.parse(line.get(1)));
-        transactionBuilder.bookingDate(LocalDate.parse(line.get(0)));
-        transactionBuilder.title(line.get(2));
-        transactionBuilder.transactioneeName(line.get(3));
-        transactionBuilder.transactioneeAccountNumber(line.get(4));
-        transactionBuilder.amount(new BigDecimal(line.get(5)));
-        transactionBuilder.balance(new BigDecimal(line.get(6)));
+        Transaction.TransactionBuilder transactionBuilder = new Transaction.TransactionBuilder()
+            .operationDate(LocalDate.parse(line.get(1)))
+            .bookingDate(LocalDate.parse(line.get(0)))
+            .title(line.get(2))
+            .transactioneeName(line.get(3))
+            .transactioneeAccountNumber(line.get(4))
+            .amount(new BigDecimal(line.get(5)))
+            .balance(new BigDecimal(line.get(6)));
 
         return transactionBuilder.build();
     }
