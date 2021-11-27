e00da8e99645dd1154882961a494c4165defc50c
Kamil153 <kamilpolak15@gmail.com>
2021-11-27 19:49:13 +0100

    add app config and initialize spring container

--- app/src/main/java/com/objectzilla/App.java
+++ app/src/main/java/com/objectzilla/App.java
@@ -3,12 +3,11 @@
  */
 package com.objectzilla;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     }
 }
