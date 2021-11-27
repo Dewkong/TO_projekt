3507d854710c38ac81c59f4758a68ad412799fb7
karkoon <kacperak23@gmail.com>
2021-11-27 19:50:25 +0100

    Change transaction list to be modifiable. Add method to add items to list.

--- app/src/main/java/com/objectzilla/model/TransactionHistory.java
+++ app/src/main/java/com/objectzilla/model/TransactionHistory.java
@@ -3,11 +3,17 @@ package com.objectzilla.model;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+
 public class TransactionHistory {
 
-    private final ObservableList<Transaction> transactions = FXCollections.emptyObservableList();
+    private final ObservableList<Transaction> transactions = FXCollections.observableList(new ArrayList<>());
 
     public ObservableList<Transaction> getTransactions() {
         return transactions;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
     }
 }
