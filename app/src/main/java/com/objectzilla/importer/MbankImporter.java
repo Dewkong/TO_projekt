ca3de5eb2e8d5df6e2ff938320d2353756a52060
Szymon Rynkowski <rynkowski.sz@gmail.com>
2021-11-28 14:23:49 +0100

    reformat

--- app/src/main/java/com/objectzilla/importer/MbankImporter.java
+++ app/src/main/java/com/objectzilla/importer/MbankImporter.java
@@ -1,26 +1,24 @@
 package com.objectzilla.importer;
 
 import com.objectzilla.model.Transaction;
-import io.reactivex.rxjava3.core.Observable;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-public class MbankImporter implements Importer{
+public class MbankImporter implements Importer {
     private final int headerLength = 38;
 
     @Override
     public Transaction importLine(List<String> line) {
         Transaction.TransactionBuilder transactionBuilder = new Transaction.TransactionBuilder()
-            .operationDate(LocalDate.parse(line.get(0)))
-            .bookingDate(LocalDate.parse(line.get(1)))
-            .title(line.get(3))
-            .transactioneeName(line.get(4))
-            .transactioneeAccountNumber(line.get(5))
-            .amount(new BigDecimal(line.get(6)))
-            .balance(new BigDecimal(line.get(7)));
+                .operationDate(LocalDate.parse(line.get(0)))
+                .bookingDate(LocalDate.parse(line.get(1)))
+                .title(line.get(3))
+                .transactioneeName(line.get(4))
+                .transactioneeAccountNumber(line.get(5))
+                .amount(new BigDecimal(line.get(6)))
+                .balance(new BigDecimal(line.get(7)));
 
         return transactionBuilder.build();
     }
