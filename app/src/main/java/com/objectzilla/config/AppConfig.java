ce30363f007b20d70233299cb68b5456ac3d9d57
Kamil153 <kamilpolak15@gmail.com>
2021-11-29 17:52:04 +0100

    register importerService in spring container

--- app/src/main/java/com/objectzilla/config/AppConfig.java
+++ app/src/main/java/com/objectzilla/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.objectzilla.config;
 
+import com.objectzilla.service.ImporterService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
@@ -8,4 +10,8 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:application.properties")
 @Import({PersistenceConfig.class, JavaFxConfig.class})
 public class AppConfig {
+    @Bean
+    public ImporterService importerService() {
+        return new ImporterService();
+    }
 }
