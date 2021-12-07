6b0d264c0aeb0e65a75caa3a31ad9e7a2484219b
Kacper Jankowski <kjankows@student.agh.edu.pl>
2021-12-07 20:30:20 +0100

    Move magic numbers to constants

--- app/src/main/java/com/objectzilla/importer/MbankImporter.java
+++ app/src/main/java/com/objectzilla/importer/MbankImporter.java
@@ -11,19 +11,25 @@ public class MbankImporter implements Importer {
     private final static int HEADER_LENGTH = 38;
     private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
 
+    private final static int OPERATION_DATE_INDEX = 0;
+    private final static int BOOKING_DATE_INDEX = 1;
+    private final static int TITLE_INDEX = 3;
+    private final static int TRANSACTIONEE_NAME_INDEX = 4;
+    private final static int TRANSACTIONEE_ACCOUNT_NUMBER_INDEX = 5;
+    private final static int AMOUNT_INDEX = 6;
+    private final static int BALANCE_INDEX = 7;
+
     @Override
     public Transaction importLine(List<String> line) {
         System.out.println(line);
         Transaction.Builder transactionBuilder = new Transaction.Builder()
-                .operationDate(LocalDate.parse(line.get(0), FORMATTER))
-                .bookingDate(LocalDate.parse(line.get(1), FORMATTER))
-                .title(line.get(3))
-                .transactioneeName(line.get(4))
-                .transactioneeAccountNumber(line.get(5))
-                .amount(new BigDecimal(line.get(6).replace(" ", "").replace(',', '.')))
-                .balance(new BigDecimal(line.get(7).replace(" ", "").replace(',', '.')));
-
-
+                .operationDate(LocalDate.parse(line.get(OPERATION_DATE_INDEX), FORMATTER))
+                .bookingDate(LocalDate.parse(line.get(BOOKING_DATE_INDEX), FORMATTER))
+                .title(line.get(TITLE_INDEX))
+                .transactioneeName(line.get(TRANSACTIONEE_NAME_INDEX))
+                .transactioneeAccountNumber(line.get(TRANSACTIONEE_ACCOUNT_NUMBER_INDEX))
+                .amount(new BigDecimal(line.get(AMOUNT_INDEX).replace(" ", "").replace(',', '.')))
+                .balance(new BigDecimal(line.get(BALANCE_INDEX).replace(" ", "").replace(',', '.')));
         return transactionBuilder.build();
     }
 
--- app/src/main/java/com/objectzilla/importer/SantanderImporter.java
+++ app/src/main/java/com/objectzilla/importer/SantanderImporter.java
@@ -11,18 +11,24 @@ public class SantanderImporter implements Importer {
     private final static int HEADER_LENGTH = 1;
     private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.MM.yyyy");
 
+    private final static int OPERATION_DATE_INDEX = 1;
+    private final static int BOOKING_DATE_INDEX = 0;
+    private final static int TITLE_INDEX = 2;
+    private final static int TRANSACTIONEE_NAME_INDEX = 3;
+    private final static int TRANSACTIONEE_ACCOUNT_NUMBER_INDEX = 4;
+    private final static int AMOUNT_INDEX = 5;
+    private final static int BALANCE_INDEX = 6;
+
     @Override
     public Transaction importLine(List<String> line) {
         Transaction.Builder transactionBuilder = new Transaction.Builder()
-                .operationDate(LocalDate.parse(line.get(1), FORMATTER))
-                .bookingDate(LocalDate.parse(line.get(0), FORMATTER))
-                .title(line.get(2))
-                .transactioneeName(line.get(3))
-                .transactioneeAccountNumber(line.get(4))
-                .amount(new BigDecimal(line.get(5).replace(" ", "").replace(',', '.')))
-                .balance(new BigDecimal(line.get(6).replace(" ", "").replace(',', '.')));
-
-
+                .operationDate(LocalDate.parse(line.get(OPERATION_DATE_INDEX), FORMATTER))
+                .bookingDate(LocalDate.parse(line.get(BOOKING_DATE_INDEX), FORMATTER))
+                .title(line.get(TITLE_INDEX))
+                .transactioneeName(line.get(TRANSACTIONEE_NAME_INDEX))
+                .transactioneeAccountNumber(line.get(TRANSACTIONEE_ACCOUNT_NUMBER_INDEX))
+                .amount(new BigDecimal(line.get(AMOUNT_INDEX).replace(" ", "").replace(',', '.')))
+                .balance(new BigDecimal(line.get(BALANCE_INDEX).replace(" ", "").replace(',', '.')));
         return transactionBuilder.build();
     }
 
